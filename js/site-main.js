0c0edbec5428f5d19e6f30cf5990bdf8f0227769
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2022-10-03 21:33:46 +0100

    Static UI almost complete

---
+++ js/site-main.js
@@ -0,0 +1,24 @@
+let app = new Vue({
+    el : "#app",
+    data : {
+        ham : "",
+        animateClass : "animate__animated animate__slideOutRight",
+    },
+    computed : {
+        guest : function (){
+            return window.localStorage.getItem("username") || "Guest User";
+        }
+    },
+    methods : {
+        updateHam : function () {
+            if(this.ham == ""){
+                this.ham = "yt-ham-active";
+                this.animateClass = "animate__animated animate__slideInRight";
+            }
+            else {
+                this.ham = "";
+                this.animateClass = "animate__animated animate__slideOutRight"
+            }
+        }
+    }
+})
